c50139381bcd164469fe58b36da25ec10cc74b13
Nitisha Bhandari <nitisha.bhandari0@gmail.com>
2021-09-08 18:58:52 -0400

    Add the extra 2d sphere

--- assignments/a0-start/Readme.md
+++ assignments/a0-start/Readme.md
@@ -1,11 +1,8 @@
 # Readme
 
 ## Sphere2D
-
-TODO: Replace this text with results
-
-* images, gifs, videos
-* brief description of what each image/gif/video shows
+![](2d_spheres.png)
+There are two 2d spheres one in front of the other. They have the same (x,y) coordinates but different z coordinates. 
 
 ## Sphere3D
 
--- assignments/a0-start/sphere2d.cpp
+++ assignments/a0-start/sphere2d.cpp
@@ -15,7 +15,12 @@ class Sphere2D : public atkui::Framework {
     double x = width() * 0.5;
     double y = height() * 0.5;
     double radius = 100; 
-    drawSphere(vec3(x,y,0), radius);
+    drawSphere(vec3(x,y,-200), radius);
+
+    // draw another sphere at center of screen
+    setColor(vec3(1,0,0));
+    double r1 = 50; 
+    drawSphere(vec3(x,y,0), r1); // keep the same center as the first sphere
   }
 };
 
