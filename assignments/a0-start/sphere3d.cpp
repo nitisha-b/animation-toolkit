aa7f4d23f6f258d196a2c02375fe55214f01e31f
Nitisha Bhandari <nitisha.bhandari0@gmail.com>
2021-09-09 03:45:34 -0400

    Sphere moves in 3d space

--- assignments/a0-start/sphere3d.cpp
+++ assignments/a0-start/sphere3d.cpp
@@ -6,14 +6,33 @@ class Sphere3D : public atkui::Framework {
  public:
   Sphere3D() : atkui::Framework(atkui::Perspective) {
   }
+  vec3 currentPos;
+  vec3 velocity;
+
+  virtual void setup(){
+    currentPos = vec3(0,0,0);
+  }
 
   virtual void scene() {
     // colors are RGB triplets in range [0,1]
     setColor(vec3(0,1,0));
 
     // draw a sphere at center of the world
-    float radius = 50.0;
-    drawSphere(vec3(0), radius);
+    float radius = 70.0;
+    drawSphere(currentPos, radius);
+  }
+
+  void keyUp(int key, int mods){
+    // Reset to center
+    if (key == GLFW_KEY_R) {
+      currentPos = vec3(0,0,0);
+    }
+
+    // Move in a random direction
+    if (key == GLFW_KEY_SPACE){
+      velocity = 500.0f * agl::randomUnitVector();
+      currentPos = currentPos + velocity * dt();
+    }
   }
 };
 
