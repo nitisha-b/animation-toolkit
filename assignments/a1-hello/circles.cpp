e643b9ae7db29b474ac5c86cb58aa2b00709efc6
Nitisha Bhandari <nitisha.bhandari0@gmail.com>
2021-09-17 06:00:56 -0400

    Circles drawn but no movement

--- assignments/a1-hello/circles.cpp
+++ assignments/a1-hello/circles.cpp
@@ -8,10 +8,50 @@ public:
    }
 
    virtual void setup() {
+      theta = 0; 
+      thetaRate = 0.02f;
+      r = 35; 
    }
 
    virtual void scene() {
+      for(int i = 0; i < 10; i++) {
+         theta += thetaRate * dt();
+         if(i % 2 > 0) {
+            theta *= -1;
+         }
+
+         setColor(pallet[i]);
+         float outerRad = i * r;
+         
+         for(int j = 0; j < 36; j++){
+            theta += thetaRate * dt();
+            float innerRadius = 12.0;
+            px = outerRad * cos(theta + j*(10*3.14/180)) + 0.5 * width();
+            py = outerRad * sin(theta + j*(10*3.14/180)) + 0.5 * height();
+            drawSphere(vec3(px, py, 0), innerRadius);
+         }
+      }
    }
+
+   float theta; 
+   float thetaRate;
+   float r; 
+   float px;
+   float py;
+
+   std::vector<vec3> pallet =
+      {
+         vec3(0,165,227)/255.0f,
+         vec3(141,215,191)/255.0f,
+         vec3(255,150,197)/255.0f,
+         vec3(255,87,104)/255.0f,
+         vec3(255,162,58)/255.0f,
+         vec3(0,165,227)/255.0f,
+         vec3(141,215,191)/255.0f,
+         vec3(255,150,197)/255.0f,
+         vec3(255,87,104)/255.0f,
+         vec3(255,162,58)/255.0f
+      };
 
 };
 
