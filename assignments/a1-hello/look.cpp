8088912bbfdc14abb04b33df5f7c583ddb331f30
Nitisha Bhandari <nitisha.bhandari0@gmail.com>
2021-09-17 13:41:22 -0400

    Pupils moving for look (incomplete)

--- assignments/a1-hello/look.cpp
+++ assignments/a1-hello/look.cpp
@@ -10,12 +10,50 @@ class Look : public atkui::Framework {
   virtual void setup() {
     _mouseX = width() * 0.5;
     _mouseY = height() * 0.5;
+    thetaLeft = 0.0;
+    thetaRight = 0.0;
+    thetaRate = 0.6;
+    mousePos = vec3(_mouseX, _mouseY, 0);
   }
 
   virtual void scene() {
+    mousePos = vec3(mousePosition().x, mousePosition().y, 0);
+    // mouseMotion(mousePosition().x, mousePosition().y);
+    mouseMove(mousePosition().x, mousePosition().y);
     vec3 target = vec3(_mouseX, _mouseY, 0);
     setColor(vec3(1,0,0));
-    drawSphere(target, 5);
+    drawSphere(target, 15);
+
+  // draw two spheres for the eyes
+    setColor(vec3(1,1,1));
+    float x1 = width() * 0.30;
+    float y1 = height() * 0.5;
+    float r1 = 150; 
+    vec3 centerL = vec3(x1,y1,0);
+    drawSphere(vec3(x1,y1,0), r1);
+    
+    float x2 = width() * 0.70;
+    float y2 = height() * 0.5;
+    float r2 = 150; 
+    vec3 centerR = vec3(x2,y2,0);
+    drawSphere(vec3(x2,y2,0), r2);
+
+    // left pupil
+    float r = 25;
+    vec3 diffL =  mousePos - centerL;
+    thetaLeft = atan2(diffL.x, diffL.y);
+    float px1 = r * cos(thetaLeft) + 0.3 * width();
+    float py1 = r * sin(thetaLeft) + 0.5 * height();
+    setColor(vec3(0, 0, 0));
+    drawSphere(vec3(px1, py1, 80), 35);
+    
+    // right pupil 
+    vec3 diffR = mousePos - centerR;
+    thetaRight = atan2(diffR.x, diffR.y);
+    float px2 = r * cos(-thetaRight) + 0.7 * width();
+    float py2 = r * sin(-thetaRight) + 0.5 * height();
+    setColor(vec3(0, 0, 0));
+    drawSphere(vec3(px2, py2, 80), 35);
   }
 
   void mouseMove(int x, int y) {
@@ -26,6 +64,10 @@ class Look : public atkui::Framework {
  private:
   int _mouseX;
   int _mouseY;
+  float thetaLeft;
+  float thetaRight;
+  float thetaRate;
+  vec3 mousePos;
 };
 
 int main(int argc, char** argv) {
