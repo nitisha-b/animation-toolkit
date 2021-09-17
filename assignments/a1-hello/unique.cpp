6145e3ef3ff5cc37825a82e3469730b06a38bd29
Nitisha Bhandari <nitisha.bhandari0@gmail.com>
2021-09-17 17:39:40 -0400

    Draft unique

--- assignments/a1-hello/unique.cpp
+++ assignments/a1-hello/unique.cpp
@@ -7,13 +7,57 @@ class Unique : public atkui::Framework {
   Unique() : atkui::Framework(atkui::Orthographic) {
   }
 
-  virtual void setup() {
-    // your code here!
-  }
+   virtual void setup() {
+      thetaP = 0.0;
+      thetaRate = 1.5;
+      thetaBody = 0.0;
+      thetaBodyRate = 1.0;
+   }
 
-  virtual void scene() {
-    // your code here!
-  }
+   virtual void scene() {
+      setColor(vec3(0, 1, 1));
+
+      // bottom object 
+      thetaBody += thetaBodyRate * dt();
+      float ax = 0.5 * width();
+      float ay = 60.0f * sin(thetaBody);
+      drawSphere(vec3(ax, ay, 0), 300);
+
+      // draw two spheres for the eyes
+      setColor(vec3(1,1,1));
+      double x1 = width() * 0.40;
+      double y1 = 60.0f * sin(thetaBody) + 30;
+      double r1 = 100; 
+      drawSphere(vec3(x1,y1,150), r1);
+      
+      double x2 = width() * 0.60;
+      double y2 = 60.0f * sin(thetaBody) + 30;
+      double r2 = 100; 
+      drawSphere(vec3(x2,y2,150), r2);
+
+      // draw the pupils
+      thetaP += thetaRate * dt();
+      float px1 = 20.0f * sin(elapsedTime()) + 0.4 * width();
+      float py1 = 60.0f * sin(thetaBody) + 30;
+      setColor(vec3(0, 0, 0));
+      drawSphere(vec3(px1, py1, 200), 50);
+
+      float px2 = 20.0f * sin(elapsedTime()) + 0.6 * width();
+      float py2 = 60.0f * sin(thetaBody) + 30;
+      setColor(vec3(0, 0, 0));
+      drawSphere(vec3(px2, py2, 200), 50);
+
+
+      drawSphere(vec3(width(), 0.5 * height(), 0), 300);
+      drawSphere(vec3(0.5 * width(), height(), 0), 300);
+      drawSphere(vec3(0, 0.5 * height(), 0), 300);
+   }
+
+   private:
+      float thetaP;
+      float thetaRate;
+      float thetaBody; 
+      float thetaBodyRate;
 };
 
 int main(int argc, char** argv) {
