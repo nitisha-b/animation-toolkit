aafb717d7ac7093078c30c6afb3b3f48fd8495b2
Nitisha Bhandari <nitisha.bhandari0@gmail.com>
2021-09-30 20:28:24 -0400

    Linear and Catmull working

--- assignments/a3-splines/interpolator_catmullrom.h
+++ assignments/a3-splines/interpolator_catmullrom.h
@@ -12,18 +12,49 @@
 class InterpolatorCatmullRom : public Interpolator
 {
 public:
-    InterpolatorCatmullRom() : Interpolator("Catmull-Rom") {}
+   InterpolatorCatmullRom() : Interpolator("Catmull-Rom") {}
 
-    virtual glm::vec3 interpolate(int segment, double u) const {
-       // todo: your code here
-       // see: interpolator.h and writeup for details
-       return glm::vec3(0);
-    }
+   virtual glm::vec3 interpolate(int segment, double u) const {
+      int n = 4*segment-1; // index
 
-    virtual void computeControlPoints(const std::vector<glm::vec3>& keys) {
-       // todo: your code here
-       // see: interpolator.h and writeup for details
-    }
+      glm::vec3 b0 = mCtrlPoints[n];
+      glm::vec3 b1 = mCtrlPoints[n+1];
+      glm::vec3 b2 = mCtrlPoints[n+2];
+      glm::vec3 b3 = mCtrlPoints[n+3];
+
+      float term1 = std::pow((1-u),3);
+      float term2 = 3 * u * std::pow((1-u),2);
+      float term3 = 3 * std::pow(u,2)*(1-u);
+      float term4 = std::pow(u,3);
+      glm::vec3 p = term1 * b0 + term2 * b1 + term3 * b2 + term4 * b3;
+      return p;
+   }
+
+   virtual void computeControlPoints(const std::vector<glm::vec3>& keys) {
+      // First segment
+      mCtrlPoints.push_back(keys[0]);
+      glm::vec3 f_b1 = keys[0] + (1.0f/6) * (keys[1] - keys[0]);
+      mCtrlPoints.push_back(f_b1);
+
+      for (int i = 1; i < keys.size()-1; i++) {
+         if (i == keys.size()-2) {
+            glm::vec3 b2 = keys[i] - (1.0f/6) * (keys[i+1] - keys[i-1]);
+            mCtrlPoints.push_back(b2);
+         }
+         else {
+            glm::vec3 b2 = keys[i+1] - (1.0f/6) * (keys[i+2] - keys[i]);
+            mCtrlPoints.push_back(b2);
+         }
+         mCtrlPoints.push_back(keys[i]);
+         glm::vec3 b1 = keys[i] + (1.0f/6) * (keys[i+1] - keys[i-1]); 
+         mCtrlPoints.push_back(b1);
+      }
+      
+      // Last segment
+      glm::vec3 l_b2 = keys[keys.size()-1] - (1.0f/6) * (keys[keys.size()-1] - keys[keys.size()-2]);
+      mCtrlPoints.push_back(l_b2);
+      mCtrlPoints.push_back(keys[keys.size()-1]);
+   }
 };
 
 #endif
--- assignments/a3-splines/interpolator_linear.h
+++ assignments/a3-splines/interpolator_linear.h
@@ -12,15 +12,18 @@
 class InterpolatorLinear : public Interpolator
 {
 public:
-    InterpolatorLinear() : Interpolator("Linear") {}
-    virtual glm::vec3 interpolate(int segment, double u) const {
-       // todo: your code here
-       return glm::vec3(0);
-    }
+   InterpolatorLinear() : Interpolator("Linear") {}
 
-    virtual void computeControlPoints(const std::vector<glm::vec3>& keys) {
-       // todo: your code here
-    }
+   virtual glm::vec3 interpolate(int segment, double u) const {
+      glm::vec3 b0 = mCtrlPoints[segment] * (float(1-u)) + mCtrlPoints[segment+ 1] * (float)u;
+      return b0;
+   }
+
+   virtual void computeControlPoints(const std::vector<glm::vec3>& keys) {
+      for (int i = 0; i < keys.size(); i++){
+         mCtrlPoints.push_back(keys[i]);
+      }
+   }
 };
 
 #endif
