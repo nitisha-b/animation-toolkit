93f5da551abf27d42b29c36cb6c6847db115ba58
Nitisha Bhandari <nitisha.bhandari0@gmail.com>
2021-10-23 04:18:52 -0400

    quat

--- libsrc/atkmath/quaternion-basecode.cpp
+++ libsrc/atkmath/quaternion-basecode.cpp
@@ -22,6 +22,7 @@ void Quaternion::toAxisAngle (Vector3& axis, double& angleRad) const
 		// axis[0] = 0; 
 		// axis[1] = 0; 
 		// axis[2] = 1;
+		std::cout << "hi" << std::endl;
 	}
 	else {
 		float sinVal = sin(angleRad/2);
@@ -30,46 +31,49 @@ void Quaternion::toAxisAngle (Vector3& axis, double& angleRad) const
 		float vZ = this->mZ/sinVal; 
 		Vector3 *a = new Vector3(vX, vY, vZ);
 		axis = *a;
-		// axis[0] = vX;
-		// axis[1] = vY; 
-		// axis[2] = vZ;
+		// std::cout << axis << std::endl;
 	} 
 }
 
 void Quaternion::fromAxisAngle (const Vector3& axis, double angleRad)
 {
-
-	this->mX = sin(angleRad/2) * axis[0];
-	this->mY = sin(angleRad/2) * axis[1];
-	this->mZ = sin(angleRad/2) * axis[2];
-	this->mW = cos(angleRad/2);
+	float vx = sin(angleRad/2) * axis[0];
+	float vy = sin(angleRad/2) * axis[1];
+	float vz = sin(angleRad/2) * axis[2];
+	float vw = cos(angleRad/2);
+	*this = Quaternion(vx, vy, vz, vw);
 }
 
 Matrix3 Quaternion::toMatrix () const
 {
 	Matrix3 m = Matrix3(); 
 
-	m[0][0] = 1 - 2 * (pow(this->mY,2) + pow(this->mZ,2));
-	m[0][1] = 2 * (this->mX * this->mY - this->mW * this->mZ);
-	m[0][2] = 2 * (this->mX * this->mZ + this->mW * this->mY);
+	float x = this->mX; 
+	float y = this->mY; 
+	float z = this->mZ;
+	float w = this->mW;
 
-	m[1][0] = 2 * (this->mX * this->mY + this->mW * this->mZ);
-	m[1][1] = 1 - 2 * (pow(this->mX,2) + pow(this->mZ,2));
-	m[1][2] = 2 * (this->mY * this->mZ - this->mW * this->mX);
+	m[0][0] = 1 - 2 * (pow(y,2) + pow(z,2));
+	m[0][1] = 2 * (x * y - w * z);
+	m[0][2] = 2 * (x * z + w * y);
 
-	m[2][0] = 2 * (this->mX * this->mZ - this->mW * this->mY);
-	m[2][1] = 2 * (this->mY * this->mZ + this->mW * this->mX);
-	m[2][2] = 1 - 2 * (pow(this->mY,2) + pow(this->mX,2));
+	m[1][0] = 2 * (x * y + w * z);
+	m[1][1] = 1 - 2 * (pow(x,2) + pow(z,2));
+	m[1][2] = 2 * (y * z - w * x);
+
+	m[2][0] = 2 * (x * z - w * y);
+	m[2][1] = 2 * (y * z + w * x);
+	m[2][2] = 1 - 2 * (pow(y,2) + pow(x,2));
 
 	return m;
 }
 
 void Quaternion::fromMatrix(const Matrix3& rot)
 {
-	float w2 = 1/4 * (1 + rot[0][0] + rot[1][1] + rot[2][2]);
-	float x2 = 1/4 * (1 + rot[0][0] - rot[1][1] - rot[2][2]);
-	float y2 = 1/4 * (1 -rot[0][0] + rot[1][1] - rot[2][2]);
-	float z2 = 1/4 * (1 -rot[0][0] - rot[1][1] + rot[2][2]);
+	float w2 = (1/4) * (1 + rot[0][0] + rot[1][1] + rot[2][2]);
+	float x2 = (1/4) * (1 + rot[0][0] - rot[1][1] - rot[2][2]);
+	float y2 = (1/4) * (1 - rot[0][0] + rot[1][1] - rot[2][2]);
+	float z2 = (1/4) * (1 - rot[0][0] - rot[1][1] + rot[2][2]);
 
 	if (w2 > x2 && w2 > y2 && w2 > z2) {
 		this->mW = sqrt(w2);
@@ -80,13 +84,13 @@ void Quaternion::fromMatrix(const Matrix3& rot)
 	else if (x2 > w2 && x2 > y2 && x2 > z2) {
 		this->mX = sqrt(x2);
 		this->mW = (1/(4 * this->mX)) * (rot[2][1] - rot[1][2]);
-		this->mY = (1/(4 * this->mX)) * (rot[1][0] - rot[0][1]);
+		this->mY = (1/(4 * this->mX)) * (rot[1][0] + rot[0][1]);
 		this->mZ = (1/(4 * this->mX)) * (rot[0][2] + rot[2][0]);
 	}
 	else if (y2 > w2 && y2 > x2 && y2 > z2) {
 		this->mY = sqrt(y2);
 		this->mW = (1/(4 * this->mY)) * (rot[0][2] - rot[2][0]);
-		this->mX = (1/(4 * this->mY)) * (rot[1][0] - rot[0][1]);
+		this->mX = (1/(4 * this->mY)) * (rot[1][0] + rot[0][1]);
 		this->mZ = (1/(4 * this->mY)) * (rot[1][2] + rot[2][1]);
 	}
 	else {	// max == z2
