0dcce64a69c7ca55585f45a8d4671a92e1acda87
Nitisha Bhandari <nitisha.bhandari0@gmail.com>
2021-11-04 22:20:23 -0400

    part 2

--- assignments/a7-motion/motion_viewer.cpp
+++ assignments/a7-motion/motion_viewer.cpp
@@ -4,6 +4,7 @@
 
 using namespace glm;
 using namespace atk;
+using namespace std;
 
 class MotionViewer : public atkui::Framework
 {
@@ -12,14 +13,37 @@ public:
    }
 
    void setup() {
+      rightBracket = false;
+      leftBracket = false;
+      periodPressed = false; 
+      commaPressed = false;  
+   }
+
+   void setFilename(string filename) {
       BVHReader reader;
-      reader.load("../motions/Beta/jump.bvh", skeleton, motion);
-      motion.update(skeleton, 0);
+      if (filename.empty() || filename == "") {
+         reader.load("../motions/Beta/jump.bvh", skeleton, motion);
+         motion.update(skeleton, 0);
+      }
+      else {
+         reader.load(filename, skeleton, motion);
+         motion.update(skeleton, 0);
+      }
    }
 
    void scene() {
-      time += dt();
-      motion.update(skeleton, time);
+      if (!paused) {
+         if (rightBracket || leftBracket) {
+            time += timeScale;
+         }
+         // else if (leftBracket) {
+         //    time 
+         // }
+         else {
+            time += dt();
+         } 
+         motion.update(skeleton, time);
+      }
 
       setColor(vec3(0,0,0.8));
       for (int i = 0; i < skeleton.getNumJoints(); i++) {
@@ -31,12 +55,54 @@ public:
          drawEllipsoid(p1, p2, 5);
       }
 
+      // if (currentFrame >= motion.getNumKeys()) {
+      //    currentFrame = 0; 
+      // }
+      // else if (!periodPressed || !commaPressed){
+      //    currentFrame++;
+      // }
+
       drawText(paused? "Paused" : "Playing", 10, 15);
       drawText("Current frame: "+std::to_string(currentFrame), 10, 35);
       drawText("Time scale: "+std::to_string(timeScale), 10, 55);
    }
 
    virtual void keyUp(int key, int mods) {
+      if (key == GLFW_KEY_P) {
+         paused = !paused;
+      }
+      if (key == GLFW_KEY_0) {
+         motion.update(skeleton, 0);
+      }
+      if (key == GLFW_KEY_PERIOD) {
+         paused = true; 
+         if (currentFrame >= motion.getNumKeys()) {
+            currentFrame = 0;
+            time = 0;
+         }
+         currentFrame++;
+         time += dt();
+         motion.update(skeleton, time);
+      }
+      if (key == GLFW_KEY_COMMA) {
+         paused = true; 
+         if (time <= 0 || currentFrame <= 0) {
+            currentFrame = motion.getNumKeys()-1;
+            time = motion.getNumKeys();
+         }
+         currentFrame--;
+         time -= dt();
+         motion.update(skeleton, time);
+      }
+      if (key == GLFW_KEY_RIGHT_BRACKET) {
+         timeScale = 1.3f * dt();
+         // motion.update(skeleton, timeScale);
+         rightBracket = true;
+      }
+      if (key == GLFW_KEY_LEFT_BRACKET) {
+         timeScale = 0.3f * dt();
+         leftBracket = true;
+      }
    }
 
 private:
@@ -47,10 +113,20 @@ private:
    int currentFrame = 0; 
    bool paused = false;
    float time = 0;
+   bool rightBracket; 
+   bool leftBracket;
+   bool commaPressed; 
+   bool periodPressed;
 };
 
 
 int main(int argc, char** argv) {
    MotionViewer viewer;
+   if (argc == 1) {
+      viewer.setFilename("");
+   }
+   else {
+      viewer.setFilename(argv[1]);
+   }
    viewer.run();
 }
