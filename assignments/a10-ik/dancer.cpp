b0f9806a0af4bb567a22269adc6f2df8c9d84470
Nitisha Bhandari <nitisha.bhandari0@gmail.com>
2021-12-04 03:57:27 -0500

    dancer

--- assignments/a10-ik/dancer.cpp
+++ assignments/a10-ik/dancer.cpp
@@ -20,6 +20,8 @@ public:
       BVHReader reader;
       reader.load("../motions/Beta/idle.bvh", _skeleton, _motion);
       _drawer.color = vec3(1,0,0);
+      thetaRate = 3.0f;
+      theta = 0.0;
    }
 
    void update()
@@ -28,13 +30,43 @@ public:
 
       IKController ik;
       // TODO: Your code here
+      int lhID = _skeleton.getByName("Beta:LeftHand")->getID();
+      int rhID = _skeleton.getByName("Beta:RightHand")->getID();
 
+      vector<Joint*> chain1; 
+      chain1.push_back(_skeleton.getByName("Beta:LeftHand"));
+      chain1.push_back(_skeleton.getByName("Beta:LeftArm"));
+      chain1.push_back(_skeleton.getByName("Beta:LeftForeArm"));
+      chain1.push_back(_skeleton.getByName("Beta:LeftShoulder"));
+
+      vector<Joint*> chain2; 
+      chain2.push_back(_skeleton.getByName("Beta:RightHand"));
+      chain2.push_back(_skeleton.getByName("Beta:RightArm"));
+      chain2.push_back(_skeleton.getByName("Beta:RightForeArm"));
+      chain2.push_back(_skeleton.getByName("Beta:RightShoulder"));
+
+      ik.solveIKCCD(_skeleton, lhID, _lhandTarget, chain1, 0.0001, 8, 0.1);
+      ik.solveIKCCD(_skeleton, rhID, _rhandTarget, chain2, 0.0001, 8, 0.1);
+
+      _skeleton.fk();
    }
 
    void scene()
    {  
       update();
       _drawer.draw(_skeleton, *this);
+      theta += thetaRate * dt();
+      float r = 35.0f;
+
+      _lhandTarget.x = 50.0;
+      _lhandTarget.y = r * sin(theta) + 110.0;
+      _lhandTarget.z = 25.0;
+
+      _rhandTarget.x = -50.0;
+      _rhandTarget.y = r * -sin(theta) + 110.0;
+      _rhandTarget.z = 25.0;
+
+
       setColor(vec3(0,0,1));
       drawSphere(_lhandTarget, 10);
       drawSphere(_rhandTarget, 10);
@@ -48,6 +80,9 @@ protected:
    // Hand target positions
    vec3 _lhandTarget;
    vec3 _rhandTarget;
+
+   float thetaRate; 
+   float theta;
 };
 
 int main(int argc, char** argv)
